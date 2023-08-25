d5a27fc6b58d27eba5c5ca18f4450c48612dcaab
ZiFeng <287851074@qq.com>
2023-08-25 17:30:03 +0800

    config🔧: 修改默认报错模板

--- config/app.php
+++ config/app.php
@@ -127,8 +127,8 @@ return [
     'route_cache_option'     => [],
 
     // 默认跳转页面对应的模板文件
-    'dispatch_success_tmpl'  => Env::get('root_path').'public/tpl/jump_sky.tpl',
-    'dispatch_error_tmpl'    => Env::get('root_path').'public/tpl/jump_sky.tpl',
+    'dispatch_success_tmpl'  => Env::get('root_path').'public/tpl/jump.tpl',
+    'dispatch_error_tmpl'    => Env::get('root_path').'public/tpl/jump.tpl',
 
     // 异常页面的模板文件
     'exception_tmpl'         => Env::get('root_path').'public/tpl/think_exception.tpl',
