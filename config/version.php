60807dc41938e262362d18230c8d00ad9c39a1e7
子枫 <287851074@qq.com>
2023-10-24 20:31:33 +0800

    config🔧: 版本

--- config/version.php
+++ config/version.php
@@ -11,7 +11,7 @@
   // | 码云:  https://gitee.com/wmc1125/zfAdmin_tpfast
   // | Mc技术论坛: http://bbs.wangmingchang.com/forum.php?mod=forumdisplay&fid=77
   // +----------------------------------------------------------------------
-  // 站点信息最后修改于 2023/10/18 20:54:56  
+  // 站点信息最后修改于 2023/10/24 20:28:40  
   
  return [
 	'version' => 'v0.231024',
@@ -25,5 +25,5 @@
 	'version_temp' => 'v0.0.0.0',
 	'pulg_data_path' => './upgrade',
 	'temp_dir_list' => '/application,/extend,/route,/thinkphp,/vendor,/theme,/public,/runtime',
-	'sql_version' => 'v0.231018',
+	'sql_version' => '',
 ];
