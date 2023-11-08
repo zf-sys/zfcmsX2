25c6414eebc5fbb07d25aa9e483f490f3bd71213
子枫 <287851074@qq.com>
2023-11-08 20:56:38 +0800

    feat✨: 新版本v0.231108

--- config/zf_auth.php
+++ config/zf_auth.php
@@ -11,12 +11,12 @@
   // | 码云:  https://gitee.com/wmc1125/zfAdmin_tpfast
   // | Mc技术论坛: http://bbs.wangmingchang.com/forum.php?mod=forumdisplay&fid=77
   // +----------------------------------------------------------------------
-  // 站点信息最后修改于 2023/10/07 08:53:22  
+  // 站点信息最后修改于 2023/11/08 20:13:48  
   
  return [
-	'email' => '287851074@qq.com',
-	'key' => '459c2e61062d15b8e79cfd8700545bb7940',
-	'sc' => 'a8fdR-0TpcBVfMREKEd5SKPQererRkFRYSk6EmSJL7n7GsFEpuwS61MDdJqpcrnZgg-S3sr1uyPY1OlropMhB1FagtIoBK8s0lW3TWPhAmR5PN8wnnu9R2aPUrieoqnI4Z7c5SIRAqZxdzODWEZKzxavDJ9zOLiinTcKuY8ujLlzlL9YREQx_vt-f3eeu03Xv_OIX3xbBtnPwBlcb2WYtHqf_plthUrQXmZ1WfgIj3PKqKR7cGmqwV9TlXsWMY1mvpdhlRRTcq_OzxDKy3QZu2DGH6h5IplkgsNdVoRjsZocDiqxT7zxXAErIPtlWPAGRjRIlINz4eb9rsuacNFH_4EpFWtkl8sZ6lDaDkUKvoKowuOBHmgRerFMumD4CmveDbo9jplq_pEzadvN3yAx-G_da88gdpGGHXy1yGgnFZk7sfatXhrri_6teAJ1QevGoHCJ7jkERwEwalJ7y-U6zuHLueSnWB9DHk-EjK_VO2v1HPQTWLDgMylICH8h8LsLmptICqViy0X0BoFog2nLJdm6mqVBt44EY-Pmu3JNFYTIvc7gMPq2A_c',
-	'site_admin_token' => '1696639869',
-	'site_token' => 'd4732b48a216bb3c69d1e50f44bcc607',
+	'email' => '',
+	'key' => '',
+	'sc' => '',
+	'site_admin_token' => '',
+	'site_token' => '',
 ];
