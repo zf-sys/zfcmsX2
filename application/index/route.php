5604d50c117754d2a34be7953d6c7f951b5e8d97
子枫 <287851074@qq.com>
2023-11-08 20:00:51 +0800

    feat✨:  前台增加动态==>静态化

--- application/index/route.php
+++ application/index/route.php
@@ -15,6 +15,31 @@ if(isset($_SERVER['REQUEST_URI'])){
 	    }
 	}
 	$val = strtolower($val);
+	//---------------增加静态化---------------
+	$is_theme_cache = ZFC('webconfig.is_theme_cache');
+	if($is_theme_cache==1){
+		//判断是否存在此文件 路径/cache/模板名/链接.html
+		$_url = $_SERVER['REQUEST_URI'];
+		//判断是否有.html后缀,如果没有则加上
+		if(strpos($_url,'.html') === false){
+			$_url = $_url.'.html';
+		}
+		$_file = './cache/'.$val.$_url;
+		$url_arr = parse_url($_url);
+		if(isset($url_arr['query'])){
+			$_filename = $url_arr['query'];
+			//$_filename不能包含\ / : * ? " < > | ,如果包含直接替换成@
+			$_filename = str_replace(['\\','/',':','*','?','"','<','>','|',','],'@',$_filename);
+			$_file = './cache/'.$val.$url_arr['path'].'/'.$_filename;
+		}
+		//判断是否存在此文件
+		if(file_exists($_file)){
+			//存在则直接输出
+			echo file_get_contents($_file);die;
+		}
+	}
+	//---------------end----------------------
+
 	//路由
 	if($val!=''){
 		$_file = './theme/'.$val.'/route.php';
