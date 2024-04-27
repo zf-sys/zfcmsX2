0263a332a2a01bf37ba72e9dcb5dd2743c1170b9
子枫 <287851074@qq.com>
2024-04-27 14:43:01 +0800

    publish🚀: v0.240427

--- config/version.php
+++ config/version.php
@@ -11,10 +11,10 @@
   // | 码云:  https://gitee.com/wmc1125/zfAdmin_tpfast
   // | Mc技术论坛: http://bbs.wangmingchang.com/forum.php?mod=forumdisplay&fid=77
   // +----------------------------------------------------------------------
-  // 站点信息最后修改于 2024/04/24 20:51:59  
+  // 站点信息最后修改于 2024/04/24 21:15:53  
   
  return [
-	'version' => 'v0.240424',
+	'version' => 'v0.240427',
 	'soft_id' => '0230873d1167beba153d1a29f88924f4',
 	'ver_name' => '子枫内容管理系统X2',
 	'ver_date' => '20220801',
