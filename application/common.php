bd2e81c086c8d89c2d6337ec5d94ae03e402cd07
子枫 <287851074@qq.com>
2024-07-09 11:26:06 +0800

    fix🐛:  修复meta数据判断diy_url存在的逻辑

--- application/common.php
+++ application/common.php
@@ -2232,8 +2232,10 @@ if(!function_exists('deal_meta_data_add')){
             unset($data['_temp_arr_key']);
         }
         //判断diy_url是否存在
-        if(db('meta_data')->where([['tb','=',$tb],['diy_url','=',$data['meta']['diy_url']],['status','=',1]])->find()){
-            return ZFRetMsg(false,'','自定义URL已存在,请换个链接');
+        if(isset($data['meta']) && is_array($data['meta'])){
+            if(db('meta_data')->where([['tb','=',$tb],['diy_url','=',$data['meta']['diy_url']],['status','=',1]])->find()){
+                return ZFRetMsg(false,'','自定义URL已存在,请换个链接');
+            }
         }
         Db::startTrans();
         try {
@@ -2283,8 +2285,10 @@ if(!function_exists('deal_meta_data_edit')){
             unset($data['_temp_arr_key']);
         }
         //判断diy_url是否存在
-        if(db('meta_data')->where([['tb','=',$tb],['diy_url','=',$data['meta']['diy_url']],['status','=',1],['post_id','<>',$data[$field_id]]])->find()){
-            return ZFRetMsg(false,'','自定义URL已存在,请换个链接');
+        if(isset($data['meta']) && is_array($data['meta'])){
+            if(db('meta_data')->where([['tb','=',$tb],['diy_url','=',$data['meta']['diy_url']],['status','=',1],['post_id','<>',$data[$field_id]]])->find()){
+                return ZFRetMsg(false,'','自定义URL已存在,请换个链接');
+            }
         }
         Db::startTrans();
         try {
