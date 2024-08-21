b0ff9e1f27e6577f42baf9aa61607ada2d83f3ab
子枫 <287851074@qq.com>
2024-08-21 14:12:53 +0800

    首页tdk

--- application/function/hook_index.php
+++ application/function/hook_index.php
@@ -6,3 +6,11 @@ add_action('home_init',function (){
         redirect(ZFC("webconfig.site_closed_url"))->send();
     }
 });
+
+//tdk
+add_action('index_tdk',function ($seo){
+    $html = '<title>'.$seo['title'].'</title>';
+    $html.= '<meta name="keywords" content="'.$seo['keywords'].'" />';
+    $html.= '<meta name="description" content="'.$seo['description'].'" />';
+    echo $html;
+});
