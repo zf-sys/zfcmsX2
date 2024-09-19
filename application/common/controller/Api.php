e712a4627ca19a8974c97585367f209504f947c2
子枫 <287851074@qq.com>
2024-09-19 10:20:08 +0800

    增加一个清缓存的功能,需登录

--- application/common/controller/Api.php
+++ application/common/controller/Api.php
@@ -9,4 +9,19 @@ class Api
     public function common_act(){
         do_action('common_act',$this);
     }
+
+    /**
+     * @return void
+     * 清除cookie
+     */
+    public function clear_cookie()
+    {
+        $admin = session('admin');
+        if(!$admin){
+            return '未登录';
+        }
+        cookie('sys_authhost_token', null, -3600);
+        // 重定向或返回响应
+        return redirect('/',302);
+    }
 }
