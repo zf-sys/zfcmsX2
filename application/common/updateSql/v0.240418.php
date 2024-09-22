5df25b7af23ffd31924aa8c1ccd02e5fc410d338
子枫 <287851074@qq.com>
2024-09-22 20:09:52 +0800

    修复sql创建菜单bug

--- application/common/updateSql/v0.240418.php
+++ application/common/updateSql/v0.240418.php
@@ -1,11 +1,11 @@
 <?php
-
-$ret_data[0] = [ 
+$pid = db('admin_role')->where('value','admin/Config/')->value('id');
+$ret_data[0] = [
     'tb_post_add',
     $this->tb_prefix.'admin_role',
-    ['name'=>'支付设置','value'=>'admin/Config/pay_config','check'=>1,'status'=>1,'summary'=>'','sort'=>40,'pid'=>154,'module'=>'admin','control'=>'Config','act'=>'pay_config','menu'=>1,'parm'=>'','token'=>time(),'icon'=>'fa fa-trello','lang'=>'','lang_pid'=>0],
+    ['name'=>'支付设置','value'=>'admin/Config/pay_config','check'=>1,'status'=>1,'summary'=>'','sort'=>40,'pid'=>$pid,'module'=>'admin','control'=>'Config','act'=>'pay_config','menu'=>1,'parm'=>'','token'=>time(),'icon'=>'fa fa-trello','lang'=>'','lang_pid'=>0],
     false,  //是否允许重复
-    ['name'=>'支付设置'], //判断条件
+    ['name'=>'支付设置','status'=>1], //判断条件
 ];
 return $ret_data;
 
--- application/common/updateSql/v0.240528.php
+++ application/common/updateSql/v0.240528.php
@@ -1,11 +1,11 @@
 <?php
-
+$pid = db('admin_role')->where('value','admin/Category/')->value('id');
 $ret_data[0] = [ 
     'tb_post_add',
     $this->tb_prefix.'admin_role',
-    ['name'=>'文章列表Pro','value'=>'admin/Category/pro_post_list','check'=>1,'status'=>1,'summary'=>'','sort'=>40,'pid'=>180,'module'=>'admin','control'=>'Category','act'=>'pro_post_list','menu'=>1,'parm'=>'','token'=>time(),'icon'=>'fa fa-book','lang'=>'','lang_pid'=>0],
+    ['name'=>'文章列表Pro','value'=>'admin/Category/pro_post_list','check'=>1,'status'=>1,'summary'=>'','sort'=>40,'pid'=>$pid,'module'=>'admin','control'=>'Category','act'=>'pro_post_list','menu'=>1,'parm'=>'','token'=>time(),'icon'=>'fa fa-book','lang'=>'','lang_pid'=>0],
     false,  //是否允许重复
-    ['name'=>'文章列表Pro'], //判断条件
+    ['name'=>'文章列表Pro','status'=>1], //判断条件
 ];
 return $ret_data;
 
