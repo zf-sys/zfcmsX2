8eea35ba2d36b08f7e2f14333bad6b790707d3cb
子枫 <287851074@qq.com>
2024-09-22 17:11:54 +0800

    与之前数据库兼容问题bug修复

--- application/common/updateSql/v0.240409.php
+++ application/common/updateSql/v0.240409.php
@@ -3,11 +3,11 @@
 $ret_data[1] = [ 
     'tb_field_edit',
     "show columns from {$this->tb_prefix}post like 'album'",
-    "alter table {$this->tb_prefix}post MODIFY  album  text(0) NOT NULL "
+    "alter table {$this->tb_prefix}post MODIFY  album  text"
 ];
 $ret_data[2] = [ 
     'tb_field_edit',
     "show columns from {$this->tb_prefix}category like 'album'",
-    "alter table {$this->tb_prefix}category MODIFY  album  text(0) NOT NULL "
+    "alter table {$this->tb_prefix}category MODIFY  album  text"
 ];
 return $ret_data;
