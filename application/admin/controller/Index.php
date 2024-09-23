bbc54bb3d669e1f38444aa70685b38610a23af3c
子枫 <287851074@qq.com>
2024-09-23 13:29:11 +0800

    ..

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -154,7 +154,7 @@ class Index extends Admin
         ];
         $admin_logo_pic = ZFC("webconfig.admin_logo_pic");
         if($admin_logo_pic==''){
-            $admin_logo_pic = '//storage-x1.90ckm.com/zfcms/logo_white.png';
+//            $admin_logo_pic = '//storage-x1.90ckm.com/zfcms/logo_white.png';
         }
         $arr['logoInfo'] = [
             'title'=>'内容管理系统',
