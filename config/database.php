be14b42f4e420e43767a0c0fb8f8180f844a9335
ZiFeng <33240850+wmc1125@users.noreply.github.com>
2024-12-20 13:48:30 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -15,9 +15,9 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'test1',
+    'database'        => '',
     // 用户名
-    'username'        => 'root',
+    'username'        => '',
     // 密码
     'password'        => '',
     // 端口
@@ -25,7 +25,7 @@ return [
     // 连接dsn
     'dsn'             => '',
     // 数据库编码默认采用utf8
-    'charset'         => 'utf8',
+    'charset'         => 'utf8mb4',
     // 数据库表前缀
     'prefix'          => 'zf_',
     // 数据库调试模式
